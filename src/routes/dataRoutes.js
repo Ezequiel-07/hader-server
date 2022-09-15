8535968ee779b8d05cfbbb62bdce09121575c794
Ezequiel-07 <90726193+Ezequiel-07@users.noreply.github.com>
2022-09-15 00:17:38 -0300

    Update dataRoutes.js

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -9,6 +9,6 @@ router.post("/api/userPost", data.postsOfUser);
 router.post("/api/getUser", data.SendUser);
 router.post("/api/verifyFollow", data.verifyFollow);
 router.post("/api/verifyBlock", data.verifyBlock);
-router.post("/v", (req, res)=>{res.send('sdasdad')});
+router.post("/v", (req, res)=>{res.json({message:'hello'})});
 
 module.exports = router;
